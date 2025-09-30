2ac178d6192b78d2a023290775104af8a891ce32
osama-fth <foutihosama@gmail.com>
2025-09-30 18:15:15 +0200

    aggiunta docker-compose

---
+++ db-init/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    cognome VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    data_di_nascita DATE NOT NULL
+);
+
+INSERT INTO users (name, cognome, email, data_di_nascita) VALUES
+('Mario','Rossi', 'mario.rossi@example.com', '1973-04-22'),
+('Luigi','Bianchi', 'luigi.bianchi@example.com', '1989-01-12'),
+('Anna','Verdi', 'anna.verdi@example.com', '1998-12-29');
